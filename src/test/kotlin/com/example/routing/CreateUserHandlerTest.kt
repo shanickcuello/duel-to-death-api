4bed12a049815bd829d9560aeba57f75d3168ea6
Shanick Gauthier Cuello <shanick.cuello@etermax.com>
2022-06-21 09:43:34 -0300

    WIP create user

---
+++ src/test/kotlin/com/example/routing/CreateUserHandlerTest.kt
@@ -0,0 +1,25 @@
+package com.example.routing
+import com.example.plugins.configureRouting
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import org.junit.jupiter.api.DisplayName
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@DisplayName("Create user should")
+
+class CreateUserHandlerTest{
+
+    @Test
+    fun exist() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/user").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("User created", bodyAsText())
+        }
+    }
+}
